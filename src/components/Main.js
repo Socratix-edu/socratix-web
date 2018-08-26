b8fd034370c4b0bdbd5b421e7b63a9a1301a6da8
Mario Villacreses <MarioVillacreses@outlook.com>
2018-08-26 13:17:58 -0400

    Add 'under construction' warning

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,11 +7,14 @@ import CourseViewer from './CourseViewer'
 export default class Main extends React.Component {
   render() {
     return (
+      <React.Fragment>
+        <p>Note: Site is still under construction</p>
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/courses" component={Courses} />
         <Route path="/learn/:id" component={CourseViewer} />
       </Switch>
+    </React.Fragment>
     )
   }
 }
